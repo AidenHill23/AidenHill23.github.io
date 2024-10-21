84065e7e009e4b31472aa08c76384fcd4f96972c
AidenHill23 <hill.aiden122@gmail.com>
2024-10-21 14:13:09 -0400

    Add files via upload

---
+++ project10/chapter10/apply/scripts/script.js
@@ -0,0 +1,10 @@
+/*Aiden Hill 10/21/24 */
+
+//Function to display content
+
+function content(){
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript Function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
